db2030e6725c19e35334bd399af3778eb1bdbd78
Correy Lim <correylim@gmail.com>
2020-11-19 14:44:56 -0800

    Extract the XMP stream from the document
    Exit the script if no XMP metadata is found in the document

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const { PDFNet } = require('@pdftron/pdfnet-node');
+const { exit } = require('process');
 
 (async () => {
   try {
     await PDFNet.initialize();
     console.log('PDFNet initialized');
+    const pdfdoc = await PDFNet.PDFDoc.createFromFilePath(
+      'PDF_with_title_and_subject.pdf'
+    );
+    const xmpStream = await (await pdfdoc.getRoot()).findObj("Metadata");
+    if (!xmpStream) {
+      console.warn('No XMP stream detected in the PDF, exitting.');
+      exit(0);
+    }
   } catch (e) {
     console.error(`Error: ${e}`);
   } finally {
